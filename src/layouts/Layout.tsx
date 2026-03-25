8837c02b1ac8df60d9242b897d7ddfaa5004afb0
TehamiKamdar <tehamikamdar15@gmail.com>
2026-03-25 17:17:21 +0500

    all things working for first template. Alhumdulillah!

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Header"
 import Footer from "../components/Footer"
 import { Outlet } from "react-router-dom"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const Layout = () =>{
   return(
     <div className="min-h-screen bg-[#0c0f0a] font-['Inter',sans-serif]">
